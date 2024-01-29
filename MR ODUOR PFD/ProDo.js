831c478632115200acfcb35e60f2509e18e7c6bf
Geofrey Onyango <geofreyonyango167@gmail.com>
2024-01-29 21:14:33 +0300

    koo

--- MR ODUOR PFD/ProDo.js
+++ MR ODUOR PFD/ProDo.js
@@ -65,9 +65,8 @@ const learners = {
     ObservationSchedules: "#", 
     AnecdotalRecords:"#", 
     AssessmentJournals:"#"
-    
-    
   },
+
   { LearningArea: "English",
       TheYear:"2024",
       Schemes: "https://docs.google.com/document/d/1BS2bynm60fnlGXHuVmCQ921hwKmm5GvI/edit?usp=sharing&ouid=110287645281482421505&rtpof=true&sd=true",
@@ -76,8 +75,53 @@ const learners = {
       ObservationSchedules: "#", 
       AnecdotalRecords:"#", 
       AssessmentJournals:"#"
-  
-  
+
+  },
+   
+    // Other learner objects without passwords
+  ],
+  GradeFive: [
+    { LearningArea: "Mathematics",
+    TheYear:"2024",
+    Schemes: "https://docs.google.com/document/d/1Bl75cTEbngbzhYkmaFcf1a-JOJIadq2z/edit?usp=sharing&ouid=110287645281482421505&rtpof=true&sd=true",
+    LessonPlans: "https://docs.google.com/document/d/1DcYB6NrPgsjo5oJviRjB1VB55dD0n6B1/edit?usp=sharing&ouid=110287645281482421505&rtpof=true&sd=true", 
+    RecordsOfWorkCovered: "https://docs.google.com/document/d/1CdfnnuPiW69jr7WfyP20RSLPRSyTF0aS/edit?usp=sharing&ouid=110287645281482421505&rtpof=true&sd=true", 
+    ObservationSchedules: "#", 
+    AnecdotalRecords:"#", 
+    AssessmentJournals:"#"
+  },
+
+  {  LearningArea: "Science and Technology",
+  TheYear:"2024",
+  Schemes: "https://docs.google.com/document/d/1B2sJXVDfTBDW83MD7DsKC3U3qzh-1JGg/edit?usp=sharing&ouid=110287645281482421505&rtpof=true&sd=true",
+  LessonPlans: "https://docs.google.com/document/d/1Dl24LQw-FDK8xbBQrMhMU9503VOj1EZG/edit?usp=sharing&ouid=110287645281482421505&rtpof=true&sd=true", 
+  RecordsOfWorkCovered: "https://docs.google.com/document/d/1CrfYeZSxhKV6LHWguTxNvPCAhsKh-g6C/edit?usp=sharing&ouid=110287645281482421505&rtpof=true&sd=true", 
+  ObservationSchedules: "#", 
+  AnecdotalRecords:"#", 
+  AssessmentJournals:"#"
+
+  },
+],
+  GradeSix: [
+    { LearningArea: "Agriculture",
+    TheYear:"2024",
+    Schemes: "https://docs.google.com/document/d/1BcNO1QzV_-oKUxEmBM7X6wjjj-y0w4Fh/edit?usp=sharing&ouid=110287645281482421505&rtpof=true&sd=true",
+    LessonPlans: "https://docs.google.com/document/d/1DlXAyfzw3lsjcLOlyc-fnY_5ImZGs9As/edit?usp=sharing&ouid=110287645281482421505&rtpof=true&sd=true", 
+    RecordsOfWorkCovered: "https://docs.google.com/document/d/1CQE0Qlsh4_E_ZlX2ucPxHVLIF6uMFSrD/edit?usp=sharing&ouid=110287645281482421505&rtpof=true&sd=true", 
+    ObservationSchedules: "#", 
+    AnecdotalRecords:"#", 
+    AssessmentJournals:"#"
+  },
+
+  {LearningArea: "Mathematics",
+  TheYear:"2024",
+  Schemes: "https://docs.google.com/document/d/1BDYfLckG8LsXpMQ_ZOWIRdycX_fTxAnW/edit?usp=sharing&ouid=110287645281482421505&rtpof=true&sd=true",
+  LessonPlans: "https://docs.google.com/document/d/1DcFrpjRSmG7DzPsv-75BsVXavEmHYWXR/edit?usp=sharing&ouid=110287645281482421505&rtpof=true&sd=true", 
+  RecordsOfWorkCovered: "https://docs.google.com/document/d/1CSqNJ-bFUqH8cCXKNFarcHdWpeTFmDcJ/edit?usp=sharing&ouid=110287645281482421505&rtpof=true&sd=true", 
+  ObservationSchedules: "https://docs.google.com/document/d/1D1Vdjzw1HpUeWXwuHhfJw7PpBqfC0Oit/edit?usp=sharing&ouid=110287645281482421505&rtpof=true&sd=true", 
+  AnecdotalRecords:"https://docs.google.com/document/d/1CzRAcg9AUZ_Xwfb5vQCwrO1gkb8yRbDB/edit?usp=sharing&ouid=110287645281482421505&rtpof=true&sd=true", 
+  AssessmentJournals:"#"
+
   },
    
     // Other learner objects without passwords
